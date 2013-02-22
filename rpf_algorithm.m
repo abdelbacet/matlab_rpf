920c472cfc8516f9eae70694fdb672ad997bf85a
panmari <smmuzi@gmail.com>
2013-02-22 14:44:35 +0100

    introduced wait bar and multithread support

--- rpf_algorithm.m
+++ rpf_algorithm.m
@@ -1,11 +1,19 @@
 %samples per pixel
+global spp;
+global bin_import; % only read from bin_import!
 spp = 8;
+
+matlabpool open
+
+progress = waitbar(0, 'zomfg..');
 for boxsize=[55 35 17 7]
     max_samples_box = boxsize^2*spp/2;
     
-    for i = 1:spp:N
-        N = preprocess_samples(i, boxsize, max_samples_box);
-        [a, b] = compute_feature_weights(boxsize, N);
-        
-    end
+    %iterate over every pixel
+    parfor i = 1:length(bin_import)/8
+        all_samples_pixel = (i-1)*8:((i-1)*8 + 7);
+        neighbourhood = preprocess_samples(all_samples_pixel, boxsize, max_samples_box, spp);
+        %[a, b] = compute_feature_weights(boxsize, N);
+        waitbar(i/length(bin_import), progress, int2str(i))
+    end 
 end
