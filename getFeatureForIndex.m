c82b39bb03b3da5fdbe5883ff6c9957611dbb8f7
panmari <smmuzi@gmail.com>
2013-02-22 15:13:58 +0100

    prettified helper function

--- getFeatureForIndex.m
+++ getFeatureForIndex.m
@@ -1,4 +1,6 @@
 function featureValue = getFeatureForIndex(f, i)
     global bin_import;
-    featureValue = [bin_import(f,i), bin_import(f + 1,i), bin_import(f + 2,i)]';
+    featureValue = [ bin_import(f,i)
+                     bin_import(f + 1,i)
+                     bin_import(f + 2,i)];
 end
