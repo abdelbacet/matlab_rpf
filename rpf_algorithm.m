fd68c66696bedf0ab2df39432275fbc059d9b09f
panmari <smmuzi@gmail.com>
2013-02-24 18:39:38 +0100

    reintroduced some changes for parallelization, disabled bar

--- rpf_algorithm.m
+++ rpf_algorithm.m
@@ -3,15 +3,19 @@ global spp;
 global bin_import; % only read from bin_import!
 spp = 8;
 
-progress = waitbar(0, 'zomfg..');
+if matlabpool('size') == 0
+    matlabpool open
+end
+%progress = waitbar(0, 'zomfg..');
 for boxsize=[55 35 17 7]
     max_samples_box = boxsize^2*spp/2;
     
     %iterate over every pixel
-    for i = 1:(length(bin_import)/8)
+    nr_pixels = length(bin_import)/8;
+    parfor i = 1:nr_pixels
         all_samples_pixel = (i-1)*8+1:((i-1)*8 + 7);
         neighbourhood = preprocess_samples(all_samples_pixel, boxsize, max_samples_box, spp);
         %[a, b] = compute_feature_weights(boxsize, N);
-        waitbar(i*8/length(bin_import), progress, int2str(i))
+        %waitbar(i*8/length(bin_import), progress, int2str(i))
     end 
 end
