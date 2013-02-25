73bb983d9f80423b5ad2d1c4937dac4dd8dd6c2e
panmari <smmuzi@gmail.com>
2013-02-25 17:59:51 +0100

    Skeleton for color sampling added

---
+++ filter_color_samples.m
@@ -0,0 +1,3 @@
+function new_colors = filter_color_samples(bin_import, all_samples_pixel, neighbourhood, a, b)
+
+end
--- rpf_algorithm.m
+++ rpf_algorithm.m
@@ -11,9 +11,14 @@ for iter_step = 1:4
     
     %iterate over every pixel
     nr_pixels = length(bin_import)/8;
+    new_colors = zeros(3, length(bin_import));
     for i = 1:nr_pixels
         all_samples_pixel = (i-1)*8+1:((i-1)*8 + 7);
         neighbourhood = preprocess_samples(bin_import, all_samples_pixel, boxsize, max_samples_box, spp);
         [a, b] = compute_feature_weights(iter_step, neighbourhood);
-    end 
+        new_colors(:, all_samples_pixel) = filter_color_samples(bin_import, all_samples_pixel, neighbourhood, a, b);
+    end
+    
+    % TODO: write new_colors back into bin_import
+    fprintf('finished iteration step!');
 end
