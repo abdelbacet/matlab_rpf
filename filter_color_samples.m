2f377e0522828a8f2acf68afa3ee716bb541f0c1
panmari <smmuzi@gmail.com>
2013-02-27 11:58:27 +0100

    introduced struct in filtering step

--- filter_color_samples.m
+++ filter_color_samples.m
@@ -7,16 +7,19 @@ function new_colors = filter_color_samples(bin_import, all_samples_pixel, neighb
     variance_color = variance/(1-sum_weights_col_rand)^2;
     variance_feature = variance_color;
     
-    color_samples = bin_import(7:9,all_samples_pixel);
-    for i=color_samples
-        new_color = zeros(3,1);
-        w = zeros(3,1);
+    new_colors = zeros(3,spp);
+    current_pixel = makeStruct(bin_import, all_samples_pixel);
+    
+    % todo: use samples_struct instead of other stuff
+    for i=1:(spp-1)
+        sum_relative_weights = zeros(3,1);
         % This loop doesn't work at all!
-        for j=neighbourhood
-            relative_weight = exp(-1/(2*variance_color)*sum(a*(j - i)^2) * ...
-                              exp(-1/(2*variance_feature)*sum(b*(1));
-            new_color = new_color + sum_weights_col_rand*j;
-%             w = w + ;
+        for j=1:length(neighbourhood.color)
+            relative_weight = exp(-1/(2*variance_color)*sum(a*(current_pixel.color(i) - neighbourhood.color(j))^2)) * ...
+                              exp(-1/(2*variance_feature)*sum(b*(current_pixel.features(i) - neighbourhood.features(j))^2));
+            new_colors(:,i) = new_colors(:,i) + relative_weight*neighbourhood.color(j);
+            sum_relative_weights = sum_relative_weights + relative_weight;
         end
+        new_colors(:,i) = new_colors(:,i)./sum_relative_weights;
     end
 end
