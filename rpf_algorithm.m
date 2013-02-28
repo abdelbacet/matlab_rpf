6966e48b3c5cd1a717867470ef1ec38d5853a247
panmari <smmuzi@gmail.com>
2013-02-28 18:55:00 +0100

    rewrote writeback into bin_import

--- rpf_algorithm.m
+++ rpf_algorithm.m
@@ -1,11 +1,14 @@
+version = '1.0';
 [bin_import, spp] = read_binary();
 
 if matlabpool('size') == 0
     matlabpool open
 end
 %progress = waitbar(0, 'zomfg..');
+%initializes random generator
+rng(42);
 boxsizes=[55 35 17 7];
-for iter_step = 1:4
+for iter_step = 4
     boxsize = boxsizes(iter_step);
     max_samples_box = boxsize^2*spp/2;
     
@@ -13,17 +16,16 @@ for iter_step = 1:4
     nr_pixels = length(bin_import)/8;
     %new_colors = zeros(3, length(bin_import));
     new_colors = zeros(length(bin_import)/8, 3, 8);
-    for i = 1:100%nr_pixels
+    parfor i = 1:nr_pixels
         all_samples_pixel = (i-1)*8+1:((i-1)*8 + 8);
         neighbourhood = preprocess_samples(bin_import, all_samples_pixel, boxsize, max_samples_box, spp);
         [a, b, weights_col_rand] = compute_feature_weights(iter_step, neighbourhood);
         new_colors(i,:,:) = filter_color_samples(bin_import, all_samples_pixel, neighbourhood, a, b, weights_col_rand, spp);
     end
     
-%     % write new_colors back into bin_import
-%     for i=1:length(bin_import)/8
-%         bin_import(7:9, (i-1)*8+1:((i-1)*8 + 8)) = new_colors(i,:,:);
-%     end
-%     fprintf('finished iteration step!');
-%     print_img(bin_import, ['iter_' num2str(iter_step)], spp);
+    % write new_colors back into bin_import 
+    % (don't question the matlab magic)
+    bin_import(7:9, :) = reshape(new_colors, [], size(new_colors, 2), 1)';
+    fprintf('finished iteration step!');
+    print_img(bin_import, ['iter_' num2str(iter_step) '_v' version], spp);
 end
