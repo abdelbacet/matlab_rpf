342a8653438ba1bf0382b345690fa5e683980ec1
panmari <smmuzi@gmail.com>
2013-02-28 18:24:56 +0100

    added unnormed color to struct for easier lookup

--- rpf_algorithm.m
+++ rpf_algorithm.m
@@ -13,18 +13,17 @@ for iter_step = 1:4
     nr_pixels = length(bin_import)/8;
     %new_colors = zeros(3, length(bin_import));
     new_colors = zeros(length(bin_import)/8, 3, 8);
-    parfor i = 1:nr_pixels
+    for i = 1:100%nr_pixels
         all_samples_pixel = (i-1)*8+1:((i-1)*8 + 8);
         neighbourhood = preprocess_samples(bin_import, all_samples_pixel, boxsize, max_samples_box, spp);
         [a, b, weights_col_rand] = compute_feature_weights(iter_step, neighbourhood);
         new_colors(i,:,:) = filter_color_samples(bin_import, all_samples_pixel, neighbourhood, a, b, weights_col_rand, spp);
-        %fprintf('pixel: %d \n', i);
     end
     
-    % write new_colors back into bin_import
-    for i=1:length(bin_import)/8
-        bin_import(7:9, (i-1)*8+1:((i-1)*8 + 8)) = new_colors(i,:,:);
-    end
-    fprintf('finished iteration step!');
-    print_img(bin_import, 'iter' + iter_step, spp);
+%     % write new_colors back into bin_import
+%     for i=1:length(bin_import)/8
+%         bin_import(7:9, (i-1)*8+1:((i-1)*8 + 8)) = new_colors(i,:,:);
+%     end
+%     fprintf('finished iteration step!');
+%     print_img(bin_import, ['iter_' num2str(iter_step)], spp);
 end
