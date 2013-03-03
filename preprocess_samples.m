887c8851c4121f4c1b40942813ab3e96dedb6a89
panmari <smmuzi@gmail.com>
2013-03-03 10:25:49 +0100

    removed secondary albedo from feature list, corrected std_deviation in preprocessing

--- preprocess_samples.m
+++ preprocess_samples.m
@@ -2,21 +2,25 @@ function neighbourhood = preprocess_samples(bin_import, k, boxsize, max_samples_
     %img_width = 362;
     mean_position = [floor(bin_import(1,k(1))), floor(bin_import(2,k(1)))]';
     standard_deviation = boxsize/4;
-    % add all samples of current pixel 
-    N = k;
+    
+
     % Normal of first intersection
     % World-Space position of first intersection
     % primary albedo (texture value of first intersection) = sec_origin
     % World-Space position of second intersection
     % secondary normal
-    % secondary albedo (texture value second intersection)
-    idx_features = [13:15, 19:21, 16:18, 21:23, 28:30, 34:36];
+
+    idx_features = [13:15, 19:21, 16:18, 21:23, 28:30];
     means        = sum(bin_import(idx_features, k), 2) / numel(k);
     st_deviation = std(bin_import(idx_features, k), 0, 2);
     % todo: weight std here as follows: 
     % world-position x 30
     % everything else x 3
-    %adapted_std = 
+    adapted_std = [ 3*st_deviation(1:3, :) 
+                    30*st_deviation(4:6,:) 
+                    3*st_deviation(7:9, :)
+                    30*st_deviation(10:12,:)
+                    3*st_deviation(13:15,:)];
     mu = repmat(mean_position, [1, max_samples_box - spp]);
     sample_pos_array = round(normrnd(mu, standard_deviation));
     % todo: rework first if into various bsxfun/logical operators:
@@ -29,10 +33,12 @@ function neighbourhood = preprocess_samples(bin_import, k, boxsize, max_samples_
     
     feature_values = bin_import(idx_features, valid_sample_idxs);
     diff_features = abs(bsxfun(@minus, feature_values, means));
-    larger_than_variance = bsxfun(@gt, feature_values, 3*st_deviation);
+    larger_than_variance = bsxfun(@gt, feature_values, adapted_std);
     variance_is_significant = diff_features > 0.1 | repmat(st_deviation > 0.1, size(valid_sample_idxs));
     chosen_samples = not(any(larger_than_variance & variance_is_significant, 1));
-    N = [N, valid_sample_idxs(chosen_samples)];
+    
+    % add all samples of current pixel and valid ones to neighbourhood
+    N = [k, valid_sample_idxs(chosen_samples)];
     %Neighbourhood ready for statistical analysis    
     neighbourhood = makeStruct(bin_import, N);
 end
