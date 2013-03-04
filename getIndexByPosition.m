55cf7a1aa7aaba7c43e2d6c8f49fa7a0f6b76450
panmari <smmuzi@gmail.com>
2013-03-04 20:14:47 +0100

    strange behaviour of getIndexByPosition fixed Oo

--- getIndexByPosition.m
+++ getIndexByPosition.m
@@ -1,5 +1,5 @@
 function index = getIndexByPosition(position)
     % y-position is multiplied by width
-    index = round(position(1))+362*round(position(2));
+    index = position(1)+362*position(2);
     index = index*8 + 1; % multiply by spp, usual adaption for matlab lists
 end
