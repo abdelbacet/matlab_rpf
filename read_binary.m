cb72c937b224b999c1ac794e9bd787f24f8dbf09
panmari <smmuzi@gmail.com>
2013-03-08 16:27:43 +0100

    added unit tests for often used functions

--- read_binary.m
+++ read_binary.m
@@ -5,7 +5,7 @@ function [bin_import, spp] = read_binary()
 
     % bin_import = single(fread (fid, [9+3*10, 362*620*8],'float'));
     % mutual information library only takes double
-    bin_import = fread (fid, [9+3*10, 362*620*8],'float');
+    bin_import = fread (fid, [9+3*10, 362*620*spp],'float');
     'finished reading'
 
     fclose(fid);
---
+++ tests/test_GetIndexByPosition.m
@@ -0,0 +1,29 @@
+function test_suite = test_GetPositionByIndex()
+    addpath('../');
+    initTestSuite;
+end
+
+function test_first_eight_samples
+    [x, y] = getPositionByIndex(1:8, 362, 8);
+    assertVectorsAlmostEqual(x, repmat(0, 1, 8));
+    assertVectorsAlmostEqual(y, repmat(0, 1, 8));
+end
+
+function test_second_eight_samples
+    [x, y] = getPositionByIndex(9:16, 362, 8);
+    assertVectorsAlmostEqual(x, repmat(1, [1, 8]));
+    assertVectorsAlmostEqual(y, repmat(0, [1, 8]));
+end
+
+function test_third_eight_samples
+    [x, y] = getPositionByIndex(17:24, 362, 8);
+    assertVectorsAlmostEqual(x, repmat(2, [1, 8]));
+    assertVectorsAlmostEqual(y, repmat(0, [1, 8]));
+end
+
+function test_pixels_second_row
+    idx = 362*8
+    [x, y] = getPositionByIndex(idx + (1:8), 362, 8);
+    assertVectorsAlmostEqual(x, repmat(0, [1, 8]));
+    assertVectorsAlmostEqual(y, repmat(1, [1, 8]));
+end
