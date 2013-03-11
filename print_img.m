77f2222dc6278cdf12eb9ef1f6be5847c7deda0c
panmari <smmuzi@gmail.com>
2013-03-11 18:21:17 +0100

    rewrote print_img to do correct thing!

--- print_img.m
+++ print_img.m
@@ -1,4 +1,4 @@
-function print_img(bin_import, name, spp)
+function img = print_img(bin_import, name, spp)
     %initialize image
     img = zeros(620, 362, 3);
     % Number of samples per pixel
@@ -6,7 +6,9 @@ function print_img(bin_import, name, spp)
     N = size(bin_import, 2);
     for i = 1:spp:N
         [x, y] = getPositionByIndex(i, size(img, 2), spp);
-        img(y + 1 , x + 1, :) = sum(bin_import(7:9,i + (0:(spp-1))),2)/spp;
+        indirect = bin_import(7:9,i + (0:(spp-1)));
+        albedo = bin_import(16:18,i + (0:(spp-1)));
+        img(y + 1 , x + 1, :) = sum(indirect.*albedo, 2)/spp;
     end
     exrwrite(img, [name '.exr']);
 end
