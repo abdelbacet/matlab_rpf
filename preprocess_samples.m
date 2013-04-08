0cc1a232ef427a73db8b58abc6e81a3cf6947ee1
panmari <smmuzi@gmail.com>
2013-04-08 11:59:50 +0200

    not making samples unique anymore, checking if sample is inside box

--- preprocess_samples.m
+++ preprocess_samples.m
@@ -11,7 +11,7 @@ function [neighbourhood, N] = preprocess_samples(bin_import, k, boxsize, max_sam
     % secondary normal => 28:30
 
     idx_features = [13:24, 28:30];
-    means        = sum(bin_import(idx_features, k), 2) / numel(k);
+    means        = sum(bin_import(idx_features, k), 2) / spp;
     st_deviation = std(bin_import(idx_features, k), 0, 2);
     % todo: weight std here as follows: 
     % world-position x 30
@@ -22,9 +22,14 @@ function [neighbourhood, N] = preprocess_samples(bin_import, k, boxsize, max_sam
     mu = repmat(mean_position, [1, max_samples_box - spp]);
     sample_pos_array = round(normrnd(mu, standard_deviation));
     % todo: rework first if into various bsxfun/logical operators:
-    inside_img = all(bsxfun(@ge, sample_pos_array, [0;0]) & bsxfun(@lt, sample_pos_array, [362; 620]), 1);
+    inside_img = all(bsxfun(@ge, sample_pos_array, [0;0]) & ...
+                     bsxfun(@lt, sample_pos_array, [362; 620]), 1);
+    % This shouldn't make too much of a difference, since the std above is
+    % chosen, so this shouldn't happen too often
+    inside_box = all(bsxfun(@ge, sample_pos_array, mean_position - boxsize) & ...
+                     bsxfun(@lt, sample_pos_array, mean_position + boxsize), 1);
     not_initial_pos = any(bsxfun(@ne, sample_pos_array, mean_position), 1);
-    valid_sample_pos = sample_pos_array(:, inside_img & not_initial_pos);
+    valid_sample_pos = sample_pos_array(:, inside_img & inside_box & not_initial_pos);
     % contains img_width, works correct
     valid_sample_idxs = valid_sample_pos(1,:) + 362*valid_sample_pos(2,:);
     valid_sample_idxs = valid_sample_idxs*spp + 1;
@@ -32,7 +37,7 @@ function [neighbourhood, N] = preprocess_samples(bin_import, k, boxsize, max_sam
     % (importance sampling)
     valid_sample_idxs = valid_sample_idxs + randi([0, 7], size(valid_sample_idxs));
     % unique could be left out, not much impact after @jl
-    valid_sample_idxs = unique(valid_sample_idxs);
+    %valid_sample_idxs = unique(valid_sample_idxs);
     feature_values = bin_import(idx_features, valid_sample_idxs);
     diff_features = abs(bsxfun(@minus, feature_values, means));
     larger_than_variance = bsxfun(@gt, diff_features, adapted_std);
