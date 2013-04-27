b7e426e9c8a6248fe1aa8081c1a0b0d9ab6f0136
panmari <smmuzi@gmail.com>
2013-04-27 11:51:53 +0200

    introduced conversion from output_color to input_color now necessary
    Could also be solved in make_struct, but imo easier this way

--- print_img.m
+++ print_img.m
@@ -1,4 +1,4 @@
-function img = print_img(bin_import, name, spp)
+function img = print_img(all_indirect, all_albedo, name, spp)
     %initialize image
     img = zeros(620, 362, 3);
     % Number of samples per pixel
@@ -6,8 +6,8 @@ function img = print_img(bin_import, name, spp)
     N = size(bin_import, 2);
     for i = 1:spp:N
         [x, y] = getPositionByIndex(i, size(img, 2), spp);
-        indirect = bin_import(7:9,i + (0:(spp-1)));
-        albedo = bin_import(16:18,i + (0:(spp-1)));
+        indirect = all_indirect(:,i + (0:(spp-1)));
+        albedo = all_albedo(:,i + (0:(spp-1)));
         img(y + 1 , x + 1, :) = sum(indirect.*albedo, 2)/spp;
     end
     exrwrite(img, [name '.exr']);
--- rpf_algorithm.m
+++ rpf_algorithm.m
@@ -36,16 +36,26 @@ nr_pixels = length(bin_import)/8;
 
 %% Compute initial energy
 initial_energy = sum(sum(bsxfun(@times, bin_import(7:9, :), [.3; .59; .11;])));
-output_colors = reshape(bin_import(7:9), [nr_pixels, 3, spp]);
+
+% Setting output color to initial values
+% TODO: check if this works with reshape to input_colors below
+input_colors = bin_import(7:9);
+output_colors = zeros(1, 3, 8);
+
 %% RPF algorithm itself
 for iter_step = 1:4
     boxsize = boxsizes(iter_step);
     max_samples_box = round(boxsize^2*spp*max_samples_factor(iter_step));
     
     
-    %new_colors = zeros(3, length(bin_import));
-    input_colors = output_colors;
-   
+    % convert output to new input if not first iteration
+    if (iter_step > 1)
+        input_colors = reshape(permute(output_colors, [2 3 1]), 3, []);
+    end
+    
+    print_img(input_colors, bin_import(16:18), ...
+            ['before_iter_' num2str(iter_step) '_' version '_npc_jlet_factor_own_mi'], spp);
+    
     % iterate over every pixel
     % This is designed to run parallel!
     parfor i = idx_min:idx_max 
@@ -63,16 +73,15 @@ for iter_step = 1:4
         output_colors(i,:,:) = filter_color_samples(neighbourhood, a, b, W_r_c, spp, debug_pixel, iter_step);
     end
     
-    % write new_colors back into bin_import 
-    % Some conversion is necessary due to parallel support of new_colors matrix
-    output_colors = permute(output_colors, [2 3 1]);
-    bin_import(7:9, :) = reshape(output_colors, 3, []);
     fprintf('finished iteration step %d ! \n', iter_step);
-    img = print_img(bin_import, ['iter_' num2str(iter_step) '_' version '_npc_jlet_factor_own_mi'], spp);
 end
 
 final_energy = sum(sum(bsxfun(@times, bin_import(7:9, :), [.3; .59; .11;])));
 fprintf('Lost %d percent energy \n', 100*(1 - final_energy/initial_energy));
+
+final_colors = reshape(permute(output_colors, [2 3 1]), 3, []);
+print_img(final_colors, bin_import(16:18), ...
+            ['before_iter_' num2str(iter_step) '_' version '_npc_jlet_factor_own_mi'], spp);
 
 
 toc
